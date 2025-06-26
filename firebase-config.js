8859bec36f91d38cb2398fc729e5c11c3a0272c5
Arnav <100010741+arnavravinder@users.noreply.github.com>
2025-06-26 12:39:37 +0000

    add env

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,11 @@
 const firebaseConfig = {
-  apiKey: getEnvVar('FIREBASE_API_KEY'),
-  authDomain: getEnvVar('FIREBASE_AUTH_DOMAIN'),
-  databaseURL: getEnvVar('FIREBASE_DATABASE_URL'),
-  projectId: getEnvVar('FIREBASE_PROJECT_ID'),
-  storageBucket: getEnvVar('FIREBASE_STORAGE_BUCKET'),
-  messagingSenderId: getEnvVar('FIREBASE_MESSAGING_SENDER_ID'),
-  appId: getEnvVar('FIREBASE_APP_ID')
+  FIREBASE_API_KEY: "AIzaSyC_SXeIsbmw97u2aklsjA0gM5WWO1_dArI",
+  FIREBASE_AUTH_DOMAIN: "reunited-web.firebaseapp.com",
+  FIREBASE_DATABASE_URL: "https://reunited-web-default-rtdb.asia-southeast1.firebasedatabase.app",
+  FIREBASE_PROJECT_ID: "reunited-web",
+  FIREBASE_STORAGE_BUCKET: "reunited-web.firebasestorage.app",
+  FIREBASE_MESSAGING_SENDER_ID: "1045353786748",
+  FIREBASE_APP_ID: "1:1045353786748:web:df81f8d326d9508d0848f9",
+  GEMINI_API_URL: "api.reunited.co.in/api/gemini",
+  FORMSPREE_URL: "https://formspree.io/f/xoveqljb"
 };
