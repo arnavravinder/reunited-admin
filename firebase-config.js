aa0ee5d4e3b7796bbf21658e4c74c920907186b3
Arnav <100010741+arnavravinder@users.noreply.github.com>
2025-06-26 12:41:45 +0000

    fix

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,8 @@
 const firebaseConfig = {
-  FIREBASE_API_KEY: "AIzaSyC_SXeIsbmw97u2aklsjA0gM5WWO1_dArI",
-  FIREBASE_AUTH_DOMAIN: "reunited-web.firebaseapp.com",
-  FIREBASE_DATABASE_URL: "https://reunited-web-default-rtdb.asia-southeast1.firebasedatabase.app",
-  FIREBASE_PROJECT_ID: "reunited-web",
-  FIREBASE_STORAGE_BUCKET: "reunited-web.firebasestorage.app",
-  FIREBASE_MESSAGING_SENDER_ID: "1045353786748",
-  FIREBASE_APP_ID: "1:1045353786748:web:df81f8d326d9508d0848f9",
-  GEMINI_API_URL: "api.reunited.co.in/api/gemini",
-  FORMSPREE_URL: "https://formspree.io/f/xoveqljb"
+    apiKey: "AIzaSyC_SXeIsbmw97u2aklsjA0gM5WWO1_dArI",
+    authDomain: "reunited-web.firebaseapp.com",
+    projectId: "reunited-web",
+    storageBucket: "reunited-web.firebasestorage.app",
+    messagingSenderId: "1045353786748",
+    appId: "1:1045353786748:web:df81f8d326d9508d0848f9"
 };
