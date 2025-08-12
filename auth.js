494548cb5ec0e6d39a6bf412a3555819b72b89a4
Arnav <100010741+arnavravinder@users.noreply.github.com>
2025-08-12 13:30:55 +0000

    feat: add dropdowns for jacket, add edit/delete/view option.

--- auth.js
+++ auth.js
@@ -1,29 +1,25 @@
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
-auth.onAuthStateChanged(user => {
-    const isLoginPage = window.location.pathname.endsWith('login.html') || window.location.pathname === '/';
-    
-    if (user) {
-        if (isLoginPage) {
-            window.location.replace('index.html');
-        } else {
-            document.body.style.display = 'block'; 
-        }
-    } else {
-        if (!isLoginPage) {
-            window.location.replace('login.html');
-        } else {
-            document.body.style.display = 'block';
-        }
-    }
-});
-
-function handleLogout() {
-    auth.signOut().catch(error => console.error('Sign out error', error));
-}
-
 document.addEventListener('DOMContentLoaded', () => {
+    auth.onAuthStateChanged(user => {
+        const isLoginPage = window.location.pathname.endsWith('login.html') || window.location.pathname.endsWith('/');
+    
+        if (user) {
+            if (isLoginPage) {
+                window.location.replace('index.html');
+            } else {
+                document.body.style.display = 'block'; 
+            }
+        } else {
+            if (!isLoginPage) {
+                window.location.replace('login.html');
+            } else {
+                document.body.style.display = 'flex';
+            }
+        }
+    });
+
     const hamburger = document.getElementById('hamburger-menu');
     const navMenu = document.getElementById('nav-menu');
     const logoutLink = document.getElementById('logout-link');
@@ -37,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if(logoutLink) {
         logoutLink.addEventListener('click', (e) => {
             e.preventDefault();
-            handleLogout();
+            auth.signOut().catch(error => console.error('Sign out error', error));
         });
     }
 });
